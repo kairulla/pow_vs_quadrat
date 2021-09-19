71ddbc7045212e09a8c2f9f6a7f7af2918e8a7d4
kairulla <kayrulla@bk.ru>
2021-09-19 14:22:14 +0600

    replace /миллисек/ to /milliseconds/

--- src/benchmark/Benchmark.java
+++ src/benchmark/Benchmark.java
@@ -4,13 +4,13 @@ public class Benchmark {
     
     public static void main(String[] args) {
         byte a = 127; // -128:127
-        int b;
+        double b;
         long start = System.currentTimeMillis(); //начало
         for (long i = 1; i < 2147483647; ++i) {
-            b = a * a;
+            b = Math.pow(a, 2);
         }
         long finish = System.currentTimeMillis(); //конец
         long elapsed = finish - start; //время
-        System.out.println(elapsed + " миллисек.");
+        System.out.println(elapsed + " milliseconds");
     }    
 }
